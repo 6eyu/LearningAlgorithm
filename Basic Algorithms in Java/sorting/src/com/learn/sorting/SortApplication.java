1b6e1bd3eefc2a7ecc6d4f0598cc47f54429d0ea
6eyu <v.liu6e@gmail.com>
2019-07-12 22:54:15 +1000

    Shell test

--- Basic Algorithms in Java/sorting/src/com/learn/sorting/SortApplication.java
+++ Basic Algorithms in Java/sorting/src/com/learn/sorting/SortApplication.java
@@ -1,7 +1,19 @@
 package com.learn.sorting;
 
+import com.learn.sorting.categories.Shell;
+
 public class SortApplication {
     public static void main(String[] args) {
+
+        Shell shell = new Shell();
+
+        String[] a = {"B","G","H","D","A","K","C","I","J","E","F"};
+
+        shell.sort(a);
+
+        for(String s : a) {
+            System.out.print(s);
+        }
 
     }
 }
