16a36dec18409fcd63b9dd66685cf4b17b518cdf
6eyu <v.liu6e@gmail.com>
2019-07-17 22:15:20 +1000

    update merge method

--- Basic Algorithms in Java/sorting/src/com/learn/sorting/categories/Merge.java
+++ Basic Algorithms in Java/sorting/src/com/learn/sorting/categories/Merge.java
@@ -1,4 +1,40 @@
 package com.learn.sorting.categories;
 
 public class Merge {
+
+    private Comparable[] aux;
+
+
+    public void topDownSort(Comparable[] a) {
+        aux = new Comparable[a.length];
+
+    }
+
+    private void topDown() {
+
+    }
+
+
+
+    private void merge(Comparable[] a, int lo, int mid, int hi) {
+
+        int i = lo;
+        int j = mid + 1;
+
+        for(int k = lo; k <= hi; k++) {
+            aux[k] = a[k];
+        }
+
+        for(int k = lo; k <= hi; k++) {
+            if      (i > mid)               a[k] = aux[j++];
+            else if (j > hi)                a[k] = aux[i++];
+            else if (less(aux[j], aux[i]))  a[k] = aux[j++];
+            else                            a[k] = aux[i++];
+        }
+    }
+
+    private boolean less(Comparable v, Comparable selected) {
+        return v.compareTo(selected) < 0;
+    }
+
 }
