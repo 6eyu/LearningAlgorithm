6183d30a051d8baea5a6adceb68504affe09a0d2
6eyu <v.liu6e@gmail.com>
2019-07-23 23:07:12 +1000

    partition

--- Basic Algorithms in Java/sorting/src/com/learn/sorting/categories/Quick.java
+++ Basic Algorithms in Java/sorting/src/com/learn/sorting/categories/Quick.java
@@ -9,8 +9,28 @@ public class Quick {
 
     }
 
-    private int partition() {
-        return 0;
+    private int partition(Comparable[] a, int lo, int hi) {
+        int i = lo;
+        int j = hi + 1;
+        Comparable v = a[lo];
+        while(true) {
+            while(less(a[++i], v)) if(i == hi) break;
+            while(less(v, a[--j])) if(j == lo) break;
+            if(i >= j) break;
+            exchange(a, i, j);
+        }
+        exchange(a, lo, j);
+        return j;
+    }
+
+    private boolean less(Comparable v, Comparable selected) {
+        return v.compareTo(selected) < 0;
+    }
+
+    private void exchange(Comparable[] a, int i, int selected) {
+        Comparable temp = a[i];
+        a[i] = a[selected];
+        a[selected] = temp;
     }
 
 }
